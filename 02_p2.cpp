0953480aa725f8d26c4802abdb2f0b0cb5ad9ec1
zakariaoulli <71717940+zakariaoulli@users.noreply.github.com>
2025-08-19 20:10:32 -0400

    day 2 part 2

---
+++ 02_p2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+
+bool is_safe(const std::vector<int> &report){
+	int difference=0;
+	bool decreasing = true , increasing = true;
+	for (int i=1;i<report.size();i++){
+		difference=report[i]- report[i-1];
+		if(difference==0 || std::abs(difference)>3){
+			return false;
+		}
+		if(difference<0) increasing = false;
+		if(difference>0) decreasing = false;
+	}
+	return decreasing || increasing;
+}
+
+int main(){
+	std::string line;
+	int total_safe=0;
+	while(std::getline(std::cin,line)){
+		std::istringstream iss(line);
+		std::vector<int> report;
+		int num;
+		while(iss>>num){
+			report.push_back(num);
+		}
+
+		if (is_safe(report)){
+			total_safe++;
+			continue;
+		}
+		for(int i=0;i<report.size();i++){
+			std::vector<int> temp = report;
+			temp.erase(temp.begin()+i);
+			if(is_safe(temp)){
+				total_safe++;
+				break;
+			}
+		}
+		
+	}
+	std::cout<<"Total amout of safe reports: "<<total_safe<<std::endl;
+}	
